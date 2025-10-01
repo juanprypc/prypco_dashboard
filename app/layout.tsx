e40c98b039053f33875c7ccf8144054e5c6bb4bd
Juan Manuel Zafra <juanmanuelzafra@prypco-mac32s-MacBook-Pro.local>
2025-10-01 15:41:32 +0400

    Add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { aeonik } from "./fonts";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className={`${aeonik.variable} antialiased`} suppressHydrationWarning>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
